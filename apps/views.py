97092876a49d81529bbecbb12bef73c3884cd781
“SSONGNOSS66” <ssongnoss66@gmail.com>
2023-03-23 11:31:01 +0900

    ADD 0322 AS

---
+++ apps/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+def today_dinner(request):
+    foods = ['치킨', '삼겹살', '짜장면', '비빔밥', '햄버거']
+    picked = random.choice(foods)
+    context = {
+        'foods': foods,
+        'picked': picked,
+    }
+    return render(request, 'apps/today_dinner.html', context)
+
+def throw(request):
+    return render(request, 'apps/throw.html')
+
+def catch(request):
+    data = request.GET.get('message')
+    context = {
+        'data': data,
+    }
+    return render(request, 'apps/catch.html', context)
+
+def lotto_create(request):
+    return render(request, 'apps/lotto_create.html')
+
+def lotto(request):
+    data = int(request.GET.get('number'))
+    LottorLi = []
+    for i in range(data):
+        LottorNum = []
+        while len(LottorNum) < 6 :
+            V = random.randint(1,45)
+            if V not in LottorNum:
+                LottorNum.append(V)
+        LottorLi.append(LottorNum)
+    context = {
+        'data': data,
+        'LottorLi': LottorLi,
+    }
+    return render(request, 'apps/lotto.html', context)
